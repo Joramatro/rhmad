99d2f38b3cf46b4a2d50cceeba04103d253125b8
Jorge <Jorge@Jorge-PC>
2013-10-14 03:33:36 +0100

    +

--- src/main/java/com/amatic/ch/dao/impl/ComentarioDaoImpl.java
+++ src/main/java/com/amatic/ch/dao/impl/ComentarioDaoImpl.java
@@ -24,7 +24,8 @@ public class ComentarioDaoImpl implements ComentarioDao {
     public List<Comentario> getUltimosComentarios() {
 
 	List<Comentario> ultimosComentarios = ofy().load()
-		.type(Comentario.class).order("-fecha").list();
+		.type(Comentario.class).filter("publicado !=", "N")
+		.order("-fecha").list();
 
 	if (ultimosComentarios.size() > 40) {
 	    ultimosComentarios = ultimosComentarios.subList(0, 40);
