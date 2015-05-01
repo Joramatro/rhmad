6b73063d7d7ca3cff3e3d659eabd65866290b7aa
Jorge <Jorge@Jorge-PC>
2015-05-01 17:55:29 +0100

    +

--- war/js/jquery.fileupload-ui.js
+++ war/js/jquery.fileupload-ui.js
@@ -44,7 +44,7 @@
             autoUpload: true,
             // The following option limits the number of files that are
             // allowed to be uploaded using this widget:
-            maxNumberOfFiles: 15,
+            maxNumberOfFiles: 30,
             // The maximum allowed file size:
             maxFileSize: 32000000,
             // The minimum allowed file size:
